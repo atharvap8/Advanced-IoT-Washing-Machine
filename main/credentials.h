549c66ba5aaebdea005ece6f9032da19c4cfa62f
Atharva Phadke <atharvaphadke8@gmail.com>
2025-07-11 16:11:15 +0530

    Update credentials.h

--- main/credentials.h
+++ main/credentials.h
@@ -3,3 +3,6 @@ const char *password = "Your Wifi Password";
 
 const char *authID = "Custom Auth ID";
 const char *authPASS = "Custom Auth PASS";
+
+#define BOT_TOKEN "Your TG Bot Token"
+#define CHAT_ID "Your TG Bot's Chat ID"
