a1c5b3fd37fb8bc32d5f052e61fd6cab1397d6d6
Atharva Phadke <atharvaphadke8@gmail.com>
2025-10-21 15:34:24 +0530

    fix: files & folders

---
+++ main/credentials.h
@@ -0,0 +1,8 @@
+const char *ssid = "Your Wifi SSID";
+const char *password = "Your Wifi Password";
+
+const char *authID = "Your Server Login ID";
+const char *authPASS = "Your Server Login Password";
+
+#define BOT_TOKEN "Your Telegram Bot Token"
+#define CHAT_ID "Your Telegram Chat ID"
